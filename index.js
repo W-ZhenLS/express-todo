c6fbdefdcf91627c2bd7cc105f32b544c60751c5
W-ZhenLS <w.zhenls20@gmail.com>
2022-06-10 17:18:32 +0800

    build: added cors support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const pool = require('./dbconfig');
 const todoItemRoute = require('./routes/todo.item');
 
 app.use(express.json());    // Parse Request Body in JSON
+app.use(cors());
 
 app.use('/todos', todoItemRoute);   // let todoItem router handle all traffic to /todos
 
